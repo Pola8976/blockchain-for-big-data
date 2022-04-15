41bde99de6ec72039e0f20d6457bc67b16b0c2c8
Amit <55236006+Pola8976@users.noreply.github.com>
2022-04-15 11:03:50 +0530

    defined column width

--- src/app/blocks/blocks.component.ts
+++ src/app/blocks/blocks.component.ts
@@ -8,14 +8,14 @@
 //   dob: string;
 //   hash: string;
 //   imageName: string;
-//   timeStamp: string;
+//   timestamp: string;
 // }
 
 // const ELEMENT_DATA: blockChain[] = [
-//   {id: 1, name: 'Hydrogen', gender: 'm', dob: 'H', hash: '3hudefbhbcjde',imageName: 'image', timeStamp: '13.12.0'},
-//   {id: 2, name: 'Helium', gender: 'm', dob: 'H', hash: '3hudefbhbcjde',imageName: 'image', timeStamp: '13.12.0'},
-//   {id: 3, name: 'Hydrogen', gender: 'nb', dob: 'H', hash: '3hudefbhbcjde',imageName: 'image', timeStamp: '13.12.0'},
-//   {id: 4, name: 'Helium', gender: 'f', dob: 'H', hash: '3hudefbhbcjde',imageName: 'image', timeStamp: '13.12.0'},
+//   {id: 1, name: 'Hydrogen', gender: 'm', dob: 'H', hash: '3hudefbhbcjde',imageName: 'image', timestamp: '13.12.0'},
+//   {id: 2, name: 'Helium', gender: 'm', dob: 'H', hash: '3hudefbhbcjde',imageName: 'image', timestamp: '13.12.0'},
+//   {id: 3, name: 'Hydrogen', gender: 'nb', dob: 'H', hash: '3hudefbhbcjde',imageName: 'image', timestamp: '13.12.0'},
+//   {id: 4, name: 'Helium', gender: 'f', dob: 'H', hash: '3hudefbhbcjde',imageName: 'image', timestamp: '13.12.0'},
 // ];
 
 // @Component({
@@ -25,52 +25,117 @@
 // })
 // export class BlocksComponent implements OnInit {
 
-//   displayedColumns: string[] = ['id', 'gender','dob','hash','imageName','timeStamp'];
+//   displayedColumns: string[] = ['id', 'gender','dob','hash','imageName','timestamp'];
 //   dataSource = ELEMENT_DATA;
 //   constructor() { }
 
 //   ngOnInit(): void {
 //   }
-  
 
 // }
 
 import { Component, OnInit, Inject } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { FileUploadService } from '../file-upload.service';
 
 export interface blockChain {
-  id: number;
-  name: string;
+  id: string;
+  fullName: string;
   gender: string;
   dob: string;
   hash: string;
   imageName: string;
-  timeStamp: string;
+  timestamp: string;
 }
 
 const ELEMENT_DATA: blockChain[] = [
-  {id: 1, name: 'Hydrogen', gender: 'm', dob: 'H', hash: '3hudefbhbcjde',imageName: 'image', timeStamp: '13.12.0'},
-  {id: 2, name: 'Helium', gender: 'm', dob: 'H', hash: '3hudefbhbcjde',imageName: 'image', timeStamp: '13.12.0'},
-  {id: 3, name: 'Hydrogen', gender: 'nb', dob: 'H', hash: '3hudefbhbcjde',imageName: 'image', timeStamp: '13.12.0'},
-  {id: 4, name: 'Helium', gender: 'f', dob: 'H', hash: '3hudefbhbcjde',imageName: 'image', timeStamp: '13.12.0'},
+  {
+    id: '1',
+    fullName: 'Hydrogen',
+    gender: 'm',
+    dob: 'H',
+    hash: '3hudefbhbcjde',
+    imageName: 'image',
+    timestamp: '13.12.0',
+  },
+  {
+    id: '2',
+    fullName: 'Helium',
+    gender: 'm',
+    dob: 'H',
+    hash: '3hudefbhbcjde',
+    imageName: 'image',
+    timestamp: '13.12.0',
+  },
+  {
+    id: '3',
+    fullName: 'Hydrogen',
+    gender: 'nb',
+    dob: 'H',
+    hash: '3hudefbhbcjde',
+    imageName: 'image',
+    timestamp: '13.12.0',
+  },
+  {
+    id: '4',
+    fullName: 'Helium',
+    gender: 'f',
+    dob: 'H',
+    hash: '3hudefbhbcjde',
+    imageName: 'image',
+    timestamp: '13.12.0',
+  },
 ];
 
 @Component({
   selector: 'app-blocks',
   templateUrl: './blocks.component.html',
-  styleUrls: ['./blocks.component.scss']
+  styleUrls: ['./blocks.component.scss'],
 })
 export class BlocksComponent implements OnInit {
+  displayedColumns: string[] = [
+    'id',
+    'fullName',
+    'gender',
+    'dob',
+    'hash',
+    // 'imageName',
+    'timestamp',
+  ];
+  // dataSource = ELEMENT_DATA;
+  dataSource: MatTableDataSource<any>;
+  transactions: any[];
 
-  displayedColumns: string[] = ['id', 'gender','dob','hash','imageName','timeStamp'];
-  dataSource = ELEMENT_DATA;
-  
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fileUploadService: FileUploadService) {
+    this.transactions = [];
+    this.dataSource = new MatTableDataSource();
   }
 
-}
+  ngOnInit(): void {
+    this.fileUploadService.getChain().subscribe((replies) => {
+      for (const reply of replies) {
+        this.transactions.push(...reply.transactions);
+      }
 
+      const sourceData: blockChain[] = [];
+      for (const tx of this.transactions) {
+        console.log(tx);
+        sourceData.push({
+          id: tx.patient.pid,
+          fullName: tx.patient.fullName,
+          gender: tx.patient.gender,
+          dob: tx.patient.dob,
+          hash: tx.signature,
+          imageName: tx.dataPath,
+          timestamp: tx.timestamp,
+        });
+      }
+
+      this.dataSource = new MatTableDataSource(sourceData);
+      console.log(this.dataSource);
+    });
+  }
+}
 
 // export class DialogOverviewExampleDialog {
 //   constructor(
@@ -82,4 +147,3 @@ export class BlocksComponent implements OnInit {
 //     this.dialogRef.close();
 //   }
 // }
-
